457e20def316e9c1ebee65bfa93d14bf063501c3
coder <huutai050798@gmail.com>
2019-09-20 20:49:05 +0700

    do something - get 3 post view articles - get radom 3 authors typical - mutation: create category

--- packages/server/graphql/schemas/user/query.resolver.js
+++ packages/server/graphql/schemas/user/query.resolver.js
@@ -1,12 +1,7 @@
-import { Users } from '../../../services';
-<<<<<<< HEAD
-import { Articles } from '../../../services';
+import { Users, Articles } from '../../../services';
 
-=======
 import { combineResolvers } from 'graphql-resolvers';
 import { checkAuthentication } from '../../libs';
-import { ADMIN } from '../../../enums/userRole';
->>>>>>> origin/develop
 
 module.exports = {
   Query: {
@@ -19,28 +14,22 @@ module.exports = {
       }
       throw new Error('Incorrect username or Password!');
     },
-<<<<<<< HEAD
     get_three_authors_typical: async () => {
       const authorsTypical = [];
-      const authorId = await Users.find({});
-      console.log(authorId);
       while (authorsTypical.length < 3) {
-        const article = await Articles.find({ author_id: authorId });
-        console.log(article);
+        const author = await Users.aggregate([{ $sample: { size: 1 } }]);
+        const article = await Articles.find({ author_id: author.id });
         if (article) {
-          authorsTypical.push(authorId);
+          authorsTypical.push(author);
         }
       }
       return authorsTypical;
-    }
-=======
-
+    },
     get_current_user: combineResolvers(
       checkAuthentication,
       (_, __, { currentUser }) => {
         return Users.findOne({ id: currentUser.id });
       }
     )
->>>>>>> origin/develop
   }
 };
